b09ddade72d09d0ed63060a8ce39de7cc5f499d7
kishore-s8 <8kishore8@gmail.com>
2025-07-15 12:53:20 +0530

    second

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/add')
+def add():
+    try:
+        a = float(request.args.get('a', 0))
+        b = float(request.args.get('b', 0))
+        return {'result': a + b}
+    except:
+        return {'error': 'Invalid input'}, 400
+
+@app.route('/')
+def health():
+    return 'Calculator API is running!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=9090)
